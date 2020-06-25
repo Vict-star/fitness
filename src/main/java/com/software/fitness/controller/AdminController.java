890c65dfde92d58653de08d6743de58e7abdd4a8
Coel <2191251862@qq.com>
2020-06-25 17:38:50 +0800

    保存

--- src/main/java/com/software/fitness/controller/AdminController.java
+++ src/main/java/com/software/fitness/controller/AdminController.java
@@ -2,21 +2,17 @@ package com.software.fitness.controller;
 
 import com.software.fitness.domain.Coach;
 import com.software.fitness.domain.Member;
-import com.software.fitness.domain.User;
 import com.software.fitness.domain.Staff;
 //import com.software.fitness.service.AdminService;
 import com.software.fitness.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.websocket.server.PathParam;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -24,9 +20,7 @@ public class AdminController {
     @Autowired
     AdminService adminService;
     @GetMapping("/coachManage")
-    public String coachManagePage(HttpServletRequest request, Model model) {
-        List<Coach> coachList=adminService.coachPage();
-        model.addAttribute("coachList",coachList);
+    public String coachManagePage(HttpServletRequest request) {
         Staff staff = (Staff) request.getSession().getAttribute("loginUser");
         if (staff == null) {
             return "Login";
@@ -47,7 +41,7 @@ public class AdminController {
 
     @GetMapping("/attendance")
     public String attendancePage(HttpServletRequest request) {
-        Staff user = (Staff) request.getSession().getAttribute("loginUser");
+       Staff user = (Staff) request.getSession().getAttribute("loginUser");
         if (user == null) {
             return "Login";
         } else {
@@ -57,41 +51,16 @@ public class AdminController {
 
     @PostMapping("/coachManage/add")
     public String addCoach(Coach coach,RedirectAttributes attributes){
-        coach.setState("在职");
-        System.out.println(coach.toString());
-        Integer id = adminService.addCoach(coach);
+//        coach.setState("在职");
+//        System.out.println(coach.toString());
+//        Integer id = adminService.addCoach(coach);
+        int id =2018306614;
         String message = "";
-        if(id!=null && id!=-1){
+//        if(id!=null && id!=-1){
             message = "添加教练成功,ID为:"+id+"。";
-        }else{
-            message = "添加失败，请稍后再次尝试！";
-        }
-        attributes.addFlashAttribute("message", message);
-        return "redirect:/admin/coachManage";
-    }
-
-    @PostMapping("/coachManage/{id}/dismiss")
-    public String dismissCoach(@PathVariable("id") Integer id,RedirectAttributes attributes){
-        boolean result = adminService.dismissCoach(id);
-        String message = "";
-        if(result){
-            message = "已离职"+id+"。";
-        }else{
-            message = "更改失败，请稍后重试！或联系管理员。";
-        }
-        attributes.addFlashAttribute("message", message);
-        return "redirect:/admin/coachManage";
-    }
-
-    @PostMapping("/coachManage/{id}/employ")
-    public String employCoach(@PathVariable("id") Integer id,RedirectAttributes attributes){
-        boolean result = adminService.employCoach(id);
-        String message = "";
-        if(result){
-            message = "已启用"+id+"。";
-        }else{
-            message = "更改失败，请稍后重试！或联系管理员。";
-        }
+//        }else{
+//            message = "添加失败，请稍后再次尝试！";
+//        }
         attributes.addFlashAttribute("message", message);
         return "redirect:/admin/coachManage";
     }
@@ -108,23 +77,17 @@ public class AdminController {
         return "redirect:/admin/memberManage";
     }
 
-    @GetMapping("/coachManage/{id}")
-    public String coachInfoModal(@PathVariable int id, Model model) {
-        Coach coach = adminService.getCoach(id);
-        model.addAttribute("coach",coach);
-        return "/admin/coachEdit";
+
+    @GetMapping("/register")
+    public String registerPage(HttpServletRequest request) {
+        Staff user = (Staff) request.getSession().getAttribute("loginUser");
+        if (user == null) {
+            return "Login";
+        } else {
+            return "admin/register";
+        }
     }
 
-    @PostMapping("/coachManage/{id}/update")
-    public String updateCoach(@PathVariable int id, Coach coach,RedirectAttributes attributes) {
-        boolean result = adminService.updateCoach(coach);
-        String message;
-        if(result){
-            message = "更新成功";
-        }else{
-            message = "更改失败，请稍后重试！或联系管理员。";
-        }
-        attributes.addFlashAttribute("message", message);
-        return "redirect:/admin/coachManage/"+id;
-    }
+
+
 }
--- src/main/java/com/software/fitness/service/Impl/AdminServiceImpl.java
+++ src/main/java/com/software/fitness/service/Impl/AdminServiceImpl.java
@@ -6,8 +6,6 @@ import com.software.fitness.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class AdminServiceImpl implements AdminService {
     @Autowired
@@ -19,38 +17,4 @@ public class AdminServiceImpl implements AdminService {
         id = coachDao.addCoach(coach);
         return id;
     }
-
-    @Override
-    public List<Coach> coachPage() {
-        return coachDao.listCoach();
-    }
-
-    @Override
-    public boolean dismissCoach(int id) {
-        Coach coach = coachDao.getCoach(id);
-        coach.setState("离职");
-        int row  = coachDao.updateCoach(coach);
-        return row == 1;
-    }
-
-    @Override
-    public boolean employCoach(Integer id) {
-        Coach coach = coachDao.getCoach(id);
-        coach.setState("在职");
-        int row  = coachDao.updateCoach(coach);
-        return row == 1;
-    }
-
-    @Override
-    public Coach getCoach(int id) {
-        return coachDao.getCoach(id);
-    }
-
-    @Override
-    public boolean updateCoach(Coach coach) {
-        int row = coachDao.updateCoach(coach);
-        return row == 1;
-    }
-
-
 }
