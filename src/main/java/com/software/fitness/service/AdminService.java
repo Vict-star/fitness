d5fc631510d04b52988ec2b0e657e70d39612ec8
ShiningZ <1357591485@qq.com>
2020-06-25 11:24:32 +0800

    保存

--- src/main/java/com/software/fitness/service/AdminService.java
+++ src/main/java/com/software/fitness/service/AdminService.java
@@ -2,7 +2,19 @@ package com.software.fitness.service;
 
 import com.software.fitness.domain.Coach;
 
+import java.util.List;
+
 
 public interface AdminService {
     Integer addCoach(Coach coach);
+
+    List<Coach> coachPage();
+
+    boolean dismissCoach(int id);
+
+    Coach getCoach(int id);
+
+    boolean updateCoach(Coach coach);
+
+    boolean employCoach(Integer id);
 }
