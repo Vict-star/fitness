78baf727bde8212356bb96abdb60f84f15dd1c2a
Yeele0712 <65998869+Yeele0712@users.noreply.github.com>
2020-06-26 13:14:08 +0800

    123456
    123456

--- src/main/java/com/software/fitness/domain/Take_course.java
+++ src/main/java/com/software/fitness/domain/Take_course.java
@@ -1,4 +1,30 @@
 package com.software.fitness.domain;
 
 public class Take_course {
+	private int course_id;
+	private int member_id;
+	
+    public int getCourse_id() {
+        return course_id;
+    }
+
+    public void setCourse_id(int ID) {
+        this.course_id = ID;
+    }
+    
+    public int getMember_id() {
+        return member_id;
+    }
+
+    public void setMember_id(int ID) {
+        this.member_id = ID;
+    }
+    
+    @Override
+    public String toString() {
+        return "Take_course{" +
+                "course_id=" + course_id +
+                ", member_id=" + member_id + 
+                '}';
+    }
 }
