d0b17914f8cc6d55bcfd4ae32d75b6b92d33c813
Yeele0712 <65998869+Yeele0712@users.noreply.github.com>
2020-06-26 11:59:50 +0800

    123456
    123456

--- src/main/java/com/software/fitness/service/AdminService.java
+++ src/main/java/com/software/fitness/service/AdminService.java
@@ -1,6 +1,9 @@
 package com.software.fitness.service;
 
 import com.software.fitness.domain.Coach;
+import com.software.fitness.domain.Course;
+import com.software.fitness.domain.Member;
+import com.software.fitness.domain.Take_course;
 
 import java.util.List;
 
@@ -17,4 +20,29 @@ public interface AdminService {
     boolean updateCoach(Coach coach);
 
     boolean employCoach(Integer id);
+    
+    
+    
+    Member getMemberById(int id);
+    
+    Course getCourseById(int id);
+    
+    List<Course> coursePage();
+    
+    Integer addCourse(Course course);
+    
+    boolean updateCourse(Course course);
+    
+    boolean deleteCourse(int id);
+    
+    List<Take_course> getTakeByMemberid(int ID);
+    
+    List<Take_course> getTakeByCourseid(int ID);
+    
+    List<Take_course> getTakeAll();
+    
+    boolean addTake(Take_course take_course);
+    
+    boolean deleteTake(Take_course take_course);
+    
 }
